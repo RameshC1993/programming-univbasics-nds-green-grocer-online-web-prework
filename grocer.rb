852e8878f84ff8f78a20f01f42ca56494407a11a
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-20 23:44:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,7 +46,7 @@ def apply_coupons(cart, coupons)
     cart_item = find_item_by_name_in_collection(coupons[i][:item], cart )
     couponed_item_name = "#{coupons[i][:item]} w/coupon"
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
-    
+    if cart_item && cart_item[i] >= 
     i += 1 
   end
 end
