7595df2111fa0299bd0e234287a6a0c78361ac24
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-20 23:36:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,8 @@ def apply_coupons(cart, coupons)
   # REMEMBER: This method **should** update cart
   i = 0 
   while i < coupons.length do 
-    cart_item = find_item_by_name_in_collection()
+    cart_item = find_item_by_name_in_collection(coupons[i][:item], cart )
+    
     i += 1 
   end
 end
