0e8d10d8453934fb7faca04265450ebaac5b9b51
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-20 23:50:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,7 +53,7 @@ def apply_coupons(cart, coupons)
       else
         cart_item_with_coupon = {
           :item => couponed_item_name,
-          :price => co
+          :price => coupons[i][:cost] / 
         }
       end
     i += 1 
