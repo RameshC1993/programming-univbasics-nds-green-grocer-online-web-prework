6f7191071235a2ceae191e1e54a16ae7b0468d21
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-20 23:31:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -24,9 +24,10 @@ def consolidate_cart(cart)
       new_cart_item[:count] += 1 
     else
       new_cart_item = {
-        :item => cart[i][:item]
-        :price => cart[i][:price]
-        :clearance => cart[i][:clearance]
+        :item => cart[i][:item],
+        :price => cart[i][:price],
+        :clearance => cart[i][:clearance],
+        
       }
     end
     i += 1 
