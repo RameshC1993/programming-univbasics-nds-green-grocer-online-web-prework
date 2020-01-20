95117ac0dd92620433292544ce389b5a11eb759a
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-20 23:18:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,8 +5,9 @@ def find_item_by_name_in_collection(name, collection)
   i = 0 
   while i < collection.length do 
     if collection[i][:item] == name
-      
+      return collection[i]
   end
+  i += 1 
 end
 
 def consolidate_cart(cart)
