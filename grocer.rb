9b47a3d68bca4a8f1aeaa366ae1012d64fa22400
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-20 22:58:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -9,17 +9,7 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  hash = {}
-  cart.each do |item_hash|
-    item_hash.each do |name, price_hash|
-      if hash[name].nil?
-        hash[name] = price_hash.merge({:count => 1})
-      else
-        hash[name][:count] += 1
-      end
-    end
-  end
-  hash
+  
 end
 
 def apply_coupons(cart, coupons)
