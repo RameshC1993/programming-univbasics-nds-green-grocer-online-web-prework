15a647b287a81fd416750bc2ef1cc47038c080f3
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-20 23:47:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,7 +49,7 @@ def apply_coupons(cart, coupons)
     if cart_item && cart_item[i] >= coupons[i][:num]
       if cart_item_with_coupon
         cart_item_with_coupon[:count] += coupons[i][:num]
-        cat
+        cart_item[:count] -= 
     i += 1 
   end
 end
